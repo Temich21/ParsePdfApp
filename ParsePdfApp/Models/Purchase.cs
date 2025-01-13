2e9c537b9049d2c7cc067fd756d24eec23443c65
Artem Rakhmatullin <Artem.Rakhmatulli@fnz.com>
2025-01-13 17:14:24 +0100

    New commit

---
+++ ParsePdfApp/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿namespace ParsePdfApp.Models
+{
+    public class Purchase
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+    }
+    public class File
+    {
+        public string FileName { get; set; }
+        public string Data { get; set; }
+
+    }
+
+    public class PurchaseWithFile : Purchase
+    {
+        public File File { get; set; }
+    }
+
+}
---
+++ ParsePdfApp/Services/PurchaseService.cs
@@ -0,0 +1,30 @@
+﻿using ParsePdfApp.Models;
+
+namespace ParsePdfApp.ServicesВ
+{
+    public class PurchaseService
+    {
+        private readonly List<Purchase> _purchases;
+
+        public PurchaseService()
+        {
+            // Инициализируем список пользователей здесь, чтобы он не требовал регистрации в DI контейнере
+            _purchases = new List<Purchase>
+            {
+            new PurchaseWithFile
+                 {
+                        Id = Guid.NewGuid(),
+                        Name = "Weekly Groceries",
+                        Date = "Mon Jan 06 2025 11:26:57 GMT+0100 (Central European Standard Time)",
+                        File = new Models.File
+                        {
+                            FileName = "receipt.pdf",
+                            Data = ""
+                        }
+                 },
+            };
+        }
+
+        public List<Purchase> getPurchaseList() { return _purchases; }
+    }
+}
